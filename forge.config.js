a40d1dce8f345cc9e8ca56e955b07871f8c8fb20
NonzeroCornet34 <ullerbri000@gmail.com>
2023-07-17 22:21:54 -0600

    1.0.1

--- forge.config.js
+++ forge.config.js
@@ -3,22 +3,22 @@ module.exports = {
     {
       name: "@electron-forge/maker-squirrel",
       config: {
-        icon: "./src/assets/images/icon.png",
-        setupIcon: "./src/assets/images/icon.png",
+        icon: "icon.png",
+        setupIcon: "icon.png",
       },
     },
     {
       name: "@electron-forge/maker-deb",
       config: {
         options: {
-          icon: "./src/assets/images/icon.png",
+          icon: "icon.png",
         },
       },
     },
     {
       name: "@electron-forge/maker-dmg",
       config: {
-        icon: "./src/assets/images/icon.png",
+        icon: "icon.png",
       },
     },
   ],
