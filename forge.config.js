06125767f1d597ca4b8898a13753462b93cfce4c
NonzeroCornet34 <ullerbri000@gmail.com>
2023-07-22 17:48:16 -0600

    Windows Support. Cuz better.

--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,12 @@
 module.exports = {
+  packagerConfig: {
+    icon: "./icon.ico",
+  },
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {
-        icon: "icon.png",
-        setupIcon: "icon.ico",
-      },
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {
-        options: {
-          icon: "icon.ico",
-        },
-      },
-    },
-    {
-      name: "@electron-forge/maker-dmg",
-      config: {
-        icon: "icon.ico",
+        setupIcon: "./icon.ico",
       },
     },
   ],
